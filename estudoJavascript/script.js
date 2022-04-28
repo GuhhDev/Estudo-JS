218bc25a36892ee1b86bbfa44503573e01d7fe12
Gustavo Henrique Batista <89535864+GuhhDev@users.noreply.github.com>
2022-04-28 17:53:54 -0300

    Add files via upload

---
+++ estudoJavascript/script.js
@@ -0,0 +1,13 @@
+var dia = new Date();
+document.body.innerHTML = "<h1> hoje é " + dia + "</h1>";
+
+var a = 2;
+var b = 2;
+var theNumbersMatch;
+
+
+pens = ["vermelha", "azul" , "verde", "roxo"];
+
+
+
+document.body.innerHTML = "<h1>" + canetas + "</h1>";
